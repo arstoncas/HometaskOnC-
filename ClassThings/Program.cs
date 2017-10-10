3a9be749bb3efb497602587318791d6a751c3bec
arstoncas <emazgutov@gmail.com>
2017-10-11 01:22:29 +0300

    Add files via upload

---
+++ ClassThings/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            var str = input.Split(',', ' ');
+            Point point = new Point();
+            point.X = Convert.ToInt32(str[0]);
+            point.Y = Convert.ToInt32(str[1]);
+
+            int result;
+            Worker worker = new Worker();
+            result = worker.FindSum(point.X) + worker.FindSum(point.Y);
+            Console.WriteLine("Сумма:" + result);
+
+
+            result = worker.FindMult(point.X, point.Y);
+            Console.WriteLine("Произведение:" + result);
+
+            string result1;
+            result1 = worker.FactAndFib(point.X, point.Y);
+            Console.WriteLine("Факториал x и последнее число Фибоначчи для y: " + result1);
+            Console.ReadKey();
+
+
+        }
+    }
+
+}
---
+++ ClassThings/Worker.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProgram
+{
+    class Worker
+    {
+        public int FindSum(int number)
+        {
+            int result;
+            result = (number + number);
+            return result;
+        }
+
+
+        public int FindMult(int first, int second)
+        {
+            int result;
+            result = (first * first) * (second * second);
+            return result;
+
+        }
+
+
+
+        public string FactAndFib(int first, int second)
+        {
+            string result;
+            string fib = "text";
+            string fact;
+
+            int factor = 1;
+            if (first < 0)
+            {
+                fact ="Отрицательное число => не существует";
+                
+            }
+            else
+            {
+                for (int i = 2; i <= first; i++)
+                {
+                    factor = factor * i;
+                }
+                fact = factor.ToString();
+            }
+
+
+            int fibonacci = 0;
+            
+            if (second < 0)
+            {
+                fib = "Отрицательное число => не существует";
+            }
+            else
+            {
+                if (second == 1)
+                {
+                    fibonacci = 0;
+                }
+                else
+                {
+                    List<int> m = new List<int>();
+                    m.Add(0);
+                    m.Add(1);
+                    for (int i = 2; i < second; i++)
+                    {
+                        m.Add(m[i - 1] + m[i - 2]);
+                        fibonacci = m[i];
+                    }
+                    fib = fibonacci.ToString();
+                }
+            }
+
+            result = fact + " и " + fib;
+            return result;
+
+        }
+    }
+}
