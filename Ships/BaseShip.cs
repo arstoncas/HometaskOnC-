e28210ba346fdf37528b4fd0adf0fd40d8242a87
arstoncas <emazgutov@gmail.com>
2017-10-22 21:57:51 +0300

    Add files via upload

---
+++ Ships/BaseShip.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProgram
+{
+    public class BaseShip
+    {
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public void whatsonlist()
+        {
+            Console.WriteLine("Наблюдаем: " + Type + "; под названием: " + Name +"; ");
+        }
+        public void Shoot()
+        {
+            Console.WriteLine("Залп!");
+        }
+    }
+    class Fregate : BaseShip
+    {
+        public void ShootEffects()
+        {
+            Console.WriteLine("Так как корабль деревянный, он загорелся");
+        }
+    }
+    class Caravelle : Fregate
+    {
+    
+    }
+    class Steamship : BaseShip
+    {
+        public void ShootEffects()
+        {
+            Console.WriteLine("Ошибочка: стрелять нечем, орудий нет.");
+        }
+    }
+}
---
+++ Ships/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fregate Nevskiy = new Fregate();
+            Nevskiy.Type = "Фрегат";
+            Nevskiy.Name = "Александр Невский";
+            Nevskiy.whatsonlist();
+            Nevskiy.Shoot();
+            Nevskiy.ShootEffects();
+            Console.WriteLine();
+
+
+            Caravelle Saint = new Caravelle();
+            Saint.Type = "Каравелла";
+            Saint.Name = "Святой Христофор";
+            Saint.whatsonlist();
+            Saint.Shoot();
+            Saint.ShootEffects();
+            Console.WriteLine();
+
+
+            Steamship Titanic = new Steamship();
+            Titanic.Type = "Пароход";
+            Titanic.Name = "Титаник";
+            Titanic.whatsonlist();
+            Titanic.Shoot();
+            Titanic.ShootEffects();
+
+            Console.ReadKey();
+        }
+    }
+
+}
